d9dc5272727df3b25bfd1ffb2e2725ab7534bdba
Teufelchen1 <bennet.blischke@outlook.com>
2023-11-04 12:41:12 +0100

    Some types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+//type length = u64;
+//type rawbytes<'a> = &'a [u8];
+//type tag = u64;
+
+enum DataItem {
+    UInt(u64), // Major 0
+    NUint(u64), // Major 1
+    //Bytes(length, rawbytes<'a>), // Major 2
+    //Text(length, &'a str), // Major 3
+    //Array(length, &'a [DataItem<'a>]), // Major 4
+    //Map<Key, Value>(length, &'a [(Key, Value)]), // Major 5
+    //Tag(tag, DataItem<'a>), // Major 6
+    Float(f64), // Major 7
+    Simple(u8), // Major 7
+    Break(), // Major 7
+}
+
+fn encode(x: u64) -> DataItem {
+    DataItem::UInt(x)
+}
+
+fn decode(x: DataItem) -> String {
+    match x {
+        DataItem::UInt(y) => y.to_string(),
+        _ => todo!(),
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello IETF: {:?}", decode(encode(13)));
 }
